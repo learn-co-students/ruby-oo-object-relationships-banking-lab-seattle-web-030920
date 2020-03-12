4453855b0374d558b355eb528a79b2db14c102c4
Your Name <you@example.com>
2020-03-12 12:42:35 -0700

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+    attr_accessor :status
+    attr_reader :name, :balance
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def balance=(balance)
+        @balance = balance
+    end
+
+
+    def deposit(money_to_add)
+        self.balance = @balance + money_to_add
+    end
+
+    def display_balance
+        "Your balance is $#{balance}."
+    end
+
+    def valid?
+        balance > 0 && status == "open"
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 
 end
