985250a6a0e445ddd8bec9a1e44a03ef5f2ee477
Bakugou808 <janu.m.sung@gmail.com>
2020-03-14 19:08:59 -0700

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
-class BankAccount
+#BankeAccount ->Transfer | Transfer -> BankAccount
 
+class BankAccount
+    attr_accessor :amount, :balance, :status
+    attr_reader :name 
+    @@all = []
+
+    def initialize(name) 
+        @name = name 
+        @balance = 1000
+        @status = "open" 
+        @@all << self 
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def deposit(dep_amount)
+        self.balance += dep_amount 
+    end
+
+    def valid?
+        @status == "open" && @balance > 0 
+    end
+
+    def close_account
+        @status = "closed"
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def transfer(receiver, amount)
+        Transfer.new(self, receiver, amount)
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,51 @@
+require 'pry'
+require_relative 'console.rb'
 class Transfer
-  # your code here
+  attr_accessor :confirmation, :status
+  attr_reader :amount, :sender, :receiver
+  @@all = []
+  @@REJECTED = "rejected"
+  @@PENDING = "pending"
+  @@COMPLETE = "complete"
+  @@REVERSED = "reversed"
+  def initialize(sender, receiver, amount) 
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+    @status = @@PENDING
+
+    @@all << self 
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def valid?
+    @receiver.valid? && @sender.valid? 
+  end 
+
+  def execute_transaction
+    if @status == @@PENDING && self.has_funds? && self.valid? 
+      @sender.balance -= @amount 
+      @receiver.balance += @amount
+      @status = @@COMPLETE
+    else 
+      @status = @@REJECTED
+      "Transaction rejected. Please check your account balance."
+    end 
+  end 
+
+  def has_funds?
+    @sender.balance > @amount 
+  end
+
+  def reverse_transfer 
+    if @status == @@COMPLETE
+      @sender.balance += @amount 
+      @receiver.balance -= @amount
+      @status = @@REVERSED
+    end 
+    @status 
+  end 
 end
